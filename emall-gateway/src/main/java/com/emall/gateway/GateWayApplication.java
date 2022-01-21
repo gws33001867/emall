99ee087e2985486e418045353821871cf1e20af6
LanceShan <965250499@qq.com>
2022-01-21 21:03:05 +0800

    Initial commit

---
+++ emall-gateway/src/main/java/com/emall/gateway/GateWayApplication.java
@@ -0,0 +1,17 @@
+package com.emall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lance
+ * @date 2021/12/24 3:13 下午
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
